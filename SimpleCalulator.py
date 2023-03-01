6bd668f3dedd19bfe59ccc908f27a10b13bc3065
nibeditans <126712682+nibeditans@users.noreply.github.com>
2023-03-02 01:44:13 +0530

    Add files via upload
    This calculator project was created using Python and provides basic arithmetic operations such as addition, subtraction, multiplication, division; it can also find the remainder and power(Exponential Function). The code is designed to be easily customizable and expandable, making it a great starting point for learning more advanced programming concepts.

---
+++ SimpleCalulator.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter your first number: "))
+Operator = input("Which operator you want to use?\nChoose one (+, -, *, /, %, **):")
+num2 = int(input("Enter your second number: "))
+
+
+
+if Operator=='+':
+    print(num1+num2)
+elif Operator=='-':
+    print(num1-num2)
+elif Operator=='*':
+    print(num1*num2)
+elif Operator=='/':
+    print(num1/num2)
+elif Operator=='%':
+    print(num1%num2)
+elif Operator=='**':
+    print(num1**num2)
+else:
+    print("Invalid Operator!")

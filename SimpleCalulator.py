28f36a3c95fa56273c9403f80436491f17918048
Nibedita Sahu <126712682+nibeditans@users.noreply.github.com>
2023-03-02 19:30:46 +0530

    Update SimpleCalulator.py

--- SimpleCalulator.py
+++ SimpleCalulator.py
@@ -3,7 +3,6 @@ Operator = input("Which operator you want to use?\nChoose one (+, -, *, /, %, **
 num2 = int(input("Enter your second number: "))
 
 
-
 if Operator=='+':
     print(num1+num2)
 elif Operator=='-':
